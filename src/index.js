267a1835377265d190ae072737be14bd2c754434
MarcoGrayW <marco@graywolf.co>
2021-09-27 18:26:51 -0600

    error hello world

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function(req, res){
-    res.send('HELLO WORLD');
+    res.send('Hello World!');
 })
 
 app.listen(3000,function()
