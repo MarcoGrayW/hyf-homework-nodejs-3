92a54d3bcdd43fcb7ee2986a82f95cc57ab5d6ab
MarcoGrayW <marco@graywolf.co>
2021-09-29 13:40:25 -0600

    user created

--- src/index.js
+++ src/index.js
@@ -12,6 +12,13 @@ app.get('/users',function(req,res){
 	res.send(users);
 })
 
+users.push({id:0, username:'Marco'}); // create user marco
+app.post('/users',function(req,res){
+	res.send(users);
+})
+app.get('/users/:id',function(req,res){
+	res.send(users);
+})
 app.listen(3000,function()
 {console.log('server is listening');
 })
