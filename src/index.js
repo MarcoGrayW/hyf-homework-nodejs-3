9beea9645e624f47c6eeb1ffb359766671a940ff
MarcoGrayW <marco@graywolf.co>
2021-09-30 13:20:42 -0600

    not found user and delete him

--- src/index.js
+++ src/index.js
@@ -18,11 +18,23 @@ app.post('/user',function(req,res){
 	const user =  users.length;
 	users.push(user); // create user 
 	res.status(200).json({id: users[user]});
-})
+});
 app.get('/user/:id',function(req,res){
-	console.log(req.params.id)
+	console.log(req.params.id);
 	res.send({id: users[Number(req.params.id)]});
+});
+
+app.delete('/user/:id', function(req,res){
+	const index = users.indexOf(Number(req.params.id));
+	if(index != -1){
+		res.sendStatus(202);
+		users.splice(index, 1);
+		res.send({id: Number(req.params.id)});
+	}else{
+		return res.sendStatus(204);
+	}
 })
+
 app.listen(3000,function()
 {console.log('server is listening');
 })
